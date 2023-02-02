098bbeab4516b5db18b1c827d56fc9ca95faa728
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2023-02-02 08:57:39 -0300

    changing package name

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+import { Box, Text, TextInput, TextInputProps } from '@bet-kit-ui/react'
 
 export default {
   title: 'Form/Text Input',
