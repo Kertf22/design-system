ce8d9048ab3f3c28701bff10248a190a652c57fc
Paulo <paulo.hercilio2015@gmail.com>
2023-02-03 12:19:43 -0300

    Adding props to Icon component

--- packages/react/src/components/Icon/index.tsx
+++ packages/react/src/components/Icon/index.tsx
@@ -1,15 +1,16 @@
+import React from 'react'
 import { IconSVG } from './style'
 import { icons } from '@bet-kit-ui/tokens'
 
-export interface IconProps {
+export interface IconProps extends React.HTMLAttributes<HTMLElement> {
   name?: keyof typeof icons
   size?: 'sm' | 'md' | 'lg'
 }
 
-export function Icon({ name, size }: IconProps) {
+export function Icon({ name, size, ...props }: IconProps) {
   if (!name) return null
 
-  return <IconSVG name={name} size={size} />
+  return <IconSVG name={name} size={size} {...props} />
 }
 
 Icon.displayName = 'Icon'
